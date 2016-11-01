2905b73e3903599813c544b8b0facddee31657cf
Jorghg <jhg1508@hotmail.com>
2016-11-01 12:25:37 +1000

    Large cleanup

--- iRecover.Droid/Resources/Resource.Designer.cs
+++ iRecover.Droid/Resources/Resource.Designer.cs
@@ -517,19 +517,16 @@ namespace iRecover.Droid
 			public const int Contactus = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int FirstView = 2130903041;
+			public const int HomeScreenView = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int HomeScreenView = 2130903042;
+			public const int MakeClaim = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int MakeClaim = 2130903043;
+			public const int SplashScreen = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int SplashScreen = 2130903044;
-			
-			// aapt resource value: 0x7f030005
-			public const int Submit = 2130903045;
+			public const int Submit = 2130903044;
 			
 			static Layout()
 			{
